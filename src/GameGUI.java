9f2d94747572e59f4eff4e478d7c13ac48f96399
UlfNyberg <72010004+UlfNyberg@users.noreply.github.com>
2020-11-30 15:10:47 +0100

    GAmeGUI class created

---
+++ src/GameGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by: Ulf Nyberg
+ * Date: 2020-11-30
+ * Time: 15:09
+ * Project: four-in-a-row
+ * Copyright: MIT
+ */
+public class GameGUI extends JFrame {
+
+
+}
