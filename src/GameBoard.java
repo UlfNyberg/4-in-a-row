8d6bfbf8a54206cf2faf4718c590fc8a4bd99a95
Peter Pilestedt <peter.pilested@gmail.com>
2020-11-30 15:10:06 +0100

    skapat GameBoard

---
+++ src/GameBoard.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameBoard {
+
+    private final int ROWS = 6;
+    private final int COLUMNS = 7;
+    public List<List<Tile>> tileList = new ArrayList<>();
+
+    public GameBoard(){
+
+    }
+
+
+}
